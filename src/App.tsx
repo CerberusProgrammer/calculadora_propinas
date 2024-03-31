caeee008e2de82513615d1714eb6a531518288a6
Omar Flores <cerberusprogrammer@gmail.com>
2024-03-31 09:37:07 -0700

    New components!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Header from "./components/Header";
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+
 function App() {
   return (
     <>
-      <h1>Hola</h1>
+      <Header title={"Calculadora"}></Header>
+
+      <main className="max-w-7xl mx-auto p-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black mb-10">Menu</h2>
+          <div className="space-y-4">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item}></MenuItem>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header({ title }: { title: string }) {
+  return (
+    <>
+      <p className="p-4 bg-teal-400 text-white font-black shadow-sm text-center text-4xl">
+        {title}
+        <p className="text-2xl font-normal">de propinas</p>
+      </p>
+    </>
+  );
+}
+
+export default Header;
